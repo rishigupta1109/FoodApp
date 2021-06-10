c0df68327331c5974ca672c764ad0c836c79ded3
Rishi <grishi634@gmail.com>
2021-06-10 13:55:45 +0530

    food app 5th commit + - added

--- src/comp/FoodItems/FoodItem.js
+++ src/comp/FoodItems/FoodItem.js
@@ -1,12 +1,12 @@
 import Context from "../Context/Context";
 import "./FoodItem.css";
-import React,{useContext,useState,useEffect} from "react";
+import React,{useContext,useState,useEffect,useRef} from "react";
 
 const FoodItem=(props)=>{
-    
+    const input=useRef();
     const ctx=useContext(Context);
     const clickHandler=()=>{
-        props.onclick({
+      if(value!==0)  {props.onclick({
             src:props.src,
             name:props.name,
             amount:value,
@@ -15,15 +15,24 @@ const FoodItem=(props)=>{
             Total:props.price*value
 
         })
-
+}
     }
-    const [value,setvalue]=useState("");
+    const [value,setvalue]=useState(0);
     const changeHandler=(e)=>{
-        setvalue(e.target.value);
-        ctx.Cart&&ctx.amountChange(e.target.name,e.target.value);
-        ctx.Cart&&props.changeHandler(e.target.name,e.target.value);
+        
         
 
+
+    }
+    const valueChanger=(e)=>{
+        if(e.target.name==="plus"){
+            input.current.value=Number(input.current.value)+1;
+        }
+        else{if(input.current.value!=='0') {input.current.value=Number(input.current.value)-1;}}
+        setvalue(input.current.value);
+        ctx.Cart&&ctx.amountChange(input.current.name,input.current.value);
+        ctx.Cart&&props.changeHandler(input.current.name,input.current.value);
+        console.log(e.target.value);
 
     }
     useEffect(()=>{if(ctx.Cart)
@@ -34,7 +43,9 @@ const FoodItem=(props)=>{
             <img id="food-img" alt="pizza" src={props.src}  ></img>
             <h2 id="food-name">{props.name}</h2>
             <h2 id="food-price">{props.price}Rs</h2>
-            {props.ordered===undefined&&<input type="number" min="0" max='100' name={props.name} value={value} onChange={changeHandler} id="amount" ></input>}
+            {props.ordered===undefined&&<div id="packetinput"><button onClick={valueChanger} name="minus" className="changebutton">-</button> 
+             <input readOnly ref={input} type="number"  min="0" max='100' name={props.name} value={value} onChange={changeHandler} id="amount" ></input>
+             <button onClick={valueChanger} name="plus" className="changebutton">+</button></div>}
             {props.ordered===true&& <h2 id="food-name">{props.amount}</h2>}
             {ctx.Cart&& <h2 id="food-price">{props.Total}Rs</h2>}
             {!ctx.Cart&&<button id="add-to-cart" onClick={clickHandler}>Add to Cart</button>}
